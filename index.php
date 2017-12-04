ff8066eb834186cf0e9031493a758ab4b36e8029
Chris Woodhave <christopherwoodhave@weber.edu>
2017-12-04 16:17:40 -0700

    Minor Stuff
    Minor Stuff added to index page

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 
 require_once  'View/_header.php';
 
-echo "<p>Welcome to the application process for supplemental course credits</p>
-        <p>Please login in or create a new account to begin</p>";
+echo "<h4>Welcome to the application process for supplemental course credits</h4>
+        <p>Please login in or create a new account to begin</p>
+        <form method='post'>
+            <label for='username'>Username: </label><input type='text' name='username' id='username'/><br/>
+            <label for='password'>Password:</label><input type='password' name='password' id='password'/><br/>
+            <input class='btn btn-primary' type='submit' value='Sign In' />
+        </form>";
 
 require_once 'View/_footer.php';
